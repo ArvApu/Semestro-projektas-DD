f5fa28a77f76dc5e881718492821ba22507eea19
Marius <marius.zilguzis@ktu.edu>
2019-04-14 22:57:31 +0300

    Done reset password function. Program do not send email with link. New password form do not have exception handler.

---
+++ src/Form/ForgotPasswordType.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marius
+ * Date: 2019-04-13
+ * Time: 11:46
+ */
+
+namespace App\Form;
+
+use App\Entity\ForgotPassword;
+use App\Entity\User;
+use function Sodium\add;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ForgotPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('submit', SubmitType::class, array(
+                'label' => 'Išsiųsti nuorodą'
+            ))
+        ;
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => ForgotPassword::class,
+        ));
+    }
+}
